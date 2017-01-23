fdab6d8c761f0c546e267504cac93b55e3dc3c92
ju1234 <596117054@qq.com>
2017-01-23 10:23:36 +0800

    注册功能   补充

--- interface/reg.js
+++ interface/reg.js
@@ -8,18 +8,27 @@ var mySql = require('../sql/index.js')
 
 function register(app) {
   app.post('/api/reg', (req, res) => {
-    let data = req.body;
-    console.log(data);
-    data.id = parseInt(Math.random() * 1000);
+      let data = req.body;
+      console.log(data);
+      data.id = parseInt(Math.random() * 1000);
 
-    mySql.insertData('user', data)
-      .then(() => {
-      //插入成功
+
+      mySql.selectData(['id'], 'user').then((msg) => {
+        let idArr = JSON.parse(msg);
+        data.id = idArr[idArr.length - 1].id+1;
+        console.log(data)
+      }).then(() => {
+        return mySql.insertData('user', data)
+      }).then(() => {
+        //插入成功
         res.json(JSON.stringify({msg: true}))
       }).catch((s) => {
-      res.json(JSON.stringify({msg: undefined}));
-    });
-  });
+
+        res.json(JSON.stringify({msg: undefined}));
+      });
+    }
+  )
+  ;
 }
 
 module.exports = register;
