69f27336186cf91c68b91cbe80026714601ba6af
ju1234 <596117054@qq.com>
2017-02-09 18:01:28 +0800

    首页获取文章数据调整

--- interface/getArticle.js
+++ interface/getArticle.js
@@ -9,12 +9,22 @@ var mySql = require('../sql');
 
 
 function getArticle(app) {
-  app.post('/api/getArticle',(req,res) => {
-    mySql.selectData('*','article')
+  app.post('/api/getArticle', (req, res) => {
+    const reqCount = req.body.reqCount;
+    console.log(reqCount)
+    mySql.selectData('*', 'article')
       .then((data) => {
-        res.json(JSON.stringify({
-          data: data
-        }))
+        let dataArr = JSON.parse(data);
+        if (dataArr.length > 20 * reqCount) {
+          res.json(JSON.stringify({
+            data: JSON.stringify(dataArr.slice((reqCount - 1) * 20, reqCount * 20))
+          }))
+        } else {
+          res.json(JSON.stringify({
+            data: JSON.stringify(dataArr.slice((reqCount - 1) * 20, dataArr.length - 1))
+          }))
+        }
+
       })
       .catch((err) => {
         console.log(err)
